5f7f1da49b69d02ccd8da0ad1320800ab36fab79
minlwin <lwin.zawmin@gmail.com>
2020-01-22 18:51:36 +0630

    Commit Mediator LiveData

---
+++ 14.MediatorLiveData/app/src/main/java/com/jdc/mediator/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.jdc.mediator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private val source1 = MutableLiveData<Int>().also {
+        it.value = 0
+    }
+
+    private val source2 = MutableLiveData<Int>().also {
+        it.value = 0
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        source1.observe(this, Observer { out1.text = it.toString() })
+        source2.observe(this, Observer { out2.text = it.toString() })
+
+        val mediator = MediatorLiveData<Int>()
+
+        mediator.addSource(source1) {
+            mediator.value = it
+        }
+        mediator.addSource(source2) {
+            mediator.value = it
+        }
+
+        mediator.observe(this, Observer {
+            out.text = it.toString()
+        })
+    }
+
+    fun change1(v:View) {
+        source1.value = Random.nextInt(1000, 10000)
+    }
+
+    fun change2(v:View) {
+        source2.value = Random.nextInt(1000, 10000)
+    }
+}
