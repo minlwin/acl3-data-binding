98219438eff3e307b6489212b2f87b4fa6e8ea4c
minlwin <lwin.zawmin@gmail.com>
2020-01-22 18:53:15 +0630

    Commit Custom LiveData

---
+++ 15.ExtendLiveData/app/src/main/java/com/jdc/extend/ConnectivityLiveData.kt
@@ -0,0 +1,38 @@
+package com.jdc.extend
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.os.Build
+import androidx.lifecycle.LiveData
+
+class ConnectivityLiveData(private val context: Context)
+    :LiveData<String>(){
+
+    private var service = context
+        .getSystemService(Context.CONNECTIVITY_SERVICE)
+            as ConnectivityManager
+
+    private var listener = object :
+        ConnectivityManager.NetworkCallback() {
+
+        override fun onAvailable(network: Network) {
+            postValue("Network is Available!")
+        }
+
+        override fun onLost(network: Network) {
+            postValue("Network has been lost!")
+        }
+    }
+
+    override fun onActive() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            service.registerDefaultNetworkCallback(listener)
+        }
+    }
+
+    override fun onInactive() {
+        service.unregisterNetworkCallback(listener)
+    }
+
+}
---
+++ 15.ExtendLiveData/app/src/main/java/com/jdc/extend/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.jdc.extend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ConnectivityLiveData(this).observe(this,
+            Observer {
+                output.text = it
+            })
+    }
+}
